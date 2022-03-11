bb01e9b5bd69eb2215163ace99a4e6bdeeaa27dc
mclbdn <michalbednar93@gmail.com>
2022-03-10 19:28:04 -0600

    Add features styles

--- client/src/components/Features.js
+++ client/src/components/Features.js
@@ -1,39 +1,54 @@
 import React from "react";
+import unresolved_tickets from "../assets/unresolved-tickets.svg";
+import assigned_tickets from "../assets/assigned-tickets.svg";
+import overall_statistics from "../assets/overall-statistics.svg";
 
 const Features = () => {
   return (
     <section className="features-section">
       <div className="single-feature">
-        <h2>
-          upTicket feels incredibly
-          <span className="light-blue-text-span"> simple</span>
-        </h2>
-        <p>
-          We believe that online tools should be powerful yet simple. That's why
-          we worked hard to make upTicket as simple as possible.
-        </p>
-        <div className="feature-img feature-img-simple"></div>
+        <div className="feature-texts">
+          <h2>
+            upTicket feels incredibly
+            <span className="light-blue-text-span"> simple</span>
+          </h2>
+          <p>
+            We believe that online tools should be powerful yet simple. That's
+            why we worked hard to make upTicket as simple as possible.
+          </p>
+        </div>
+        <div className="feature-img feature-img-simple">
+          <img src={assigned_tickets} alt="" />
+        </div>
+      </div>
+      <div className="single-feature feature-reversed">
+        <div className="feature-texts">
+          <h2>
+            Resolve all your tickets for
+            <span className="pink-text-span"> free</span>
+          </h2>
+          <p>
+            Fancy saving your operational costs? upTicket is completely free for
+            teams up to 3 team members.
+          </p>
+        </div>
+        <div className="feature-img feature-img-free">
+          <img src={unresolved_tickets} alt="" />
+        </div>
       </div>
       <div className="single-feature">
-        <h2>
-          Resolve all your tickets for
-          <span className="pink-text-span"> free</span>
-        </h2>
-        <p>
-          Fancy saving your operational costs? upTicket is completely free for
-          teams up to 3 team members.
-        </p>
-        <div className="feature-img feature-img-free"></div>
-      </div>
-      <div className="single-feature">
-        <h2>
-          <span className="blue-text-span">Speed up </span>your workflow
-        </h2>
-        <p>
-          No one likes to waste their time. With upTicket, your team's workflow
-          speed increases dramatically.
-        </p>
-        <div className="feature-img feature-img-speed"></div>
+        <div className="feature-texts">
+          <h2>
+            <span className="blue-text-span">Speed up </span>your workflow
+          </h2>
+          <p>
+            No one likes to waste their time. With upTicket, your team's
+            workflow speed increases dramatically.
+          </p>
+        </div>
+        <div className="feature-img feature-img-speed">
+          <img src={overall_statistics} alt="" />
+        </div>
       </div>
     </section>
   );
