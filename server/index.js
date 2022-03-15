588e47485423a3b961bdb7fb507a428808a0cf27
mclbdn <michalbednar93@gmail.com>
2022-03-15 11:10:51 -0600

    Set up register router

--- server/index.js
+++ server/index.js
@@ -1,5 +1,13 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/api/register", (req, res) => {
+  console.log(req.body)
+});
 
 app.get("/hello", (req, res) => {
   res.send("Hello");
