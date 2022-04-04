ce229853f97a4b3538efcdaa0c6729c33c5087fb
mclbdn <michalbednar93@gmail.com>
2022-04-04 10:35:55 -0500

    Add testimonials section styles as a css(scss) module

--- client/src/components/SingleTestimonial.js
+++ client/src/components/SingleTestimonial.js
@@ -1,16 +1,27 @@
 import React from "react";
+import styles from "./SingleTestimonial.module.scss";
 
 const SingleTestimonial = ({ text, name, role, photo, icon_class, icon }) => {
   return (
-    <div className="single-testimonial">
-      <img draggable="false" className={`testimonial-icon ${icon_class}`} src={icon} alt="" />
-      <p className="testimonial-text">"{text}"</p>
+    <div className={styles.single_testimonial}>
+      <img
+        draggable="false"
+        className={`${styles[icon_class]}`}
+        src={icon}
+        alt=""
+      />
+      <p className={styles.testimonial_text}>"{text}"</p>
       <hr />
-      <div className="underline-details">
-        <img draggable="false" className="testimonial-photo" src={photo} alt="" />
-        <div className="name-and-role">
-          <p className="testimonial-name">{name}</p>
-          <p className="testimonial-role">{role}</p>
+      <div className={styles.underline_details}>
+        <img
+          draggable="false"
+          className={styles.testimonial_photo}
+          src={photo}
+          alt=""
+        />
+        <div className={styles.name_and_role}>
+          <p className={styles.testimonial_name}>{name}</p>
+          <p className={styles.testimonial_role}>{role}</p>
         </div>
       </div>
     </div>
--- client/src/components/Testimonials.js
+++ client/src/components/Testimonials.js
@@ -3,9 +3,10 @@ import SingleTestimonial from "./SingleTestimonial";
 import adriana_photo from "../assets/adriana-photo.svg";
 import josh_photo from "../assets/josh-photo.svg";
 import kate_photo from "../assets/kate-photo.svg";
-import heart_icon from "../assets/heart-icon.svg"
+import heart_icon from "../assets/heart-icon.svg";
 import star_icon from "../assets/star-icon.svg";
 import plus_one_icon from "../assets/plus-one-icon.svg";
+import styles from "./Testimonials.module.scss";
 
 const Testimonials = () => {
   const testimonials = [
@@ -15,7 +16,7 @@ const Testimonials = () => {
       role: "CEO at PayMasters",
       photo: adriana_photo,
       icon_class: "heart",
-      icon: heart_icon
+      icon: heart_icon,
     },
     {
       text: "I am deeply impressed how well upTicket works. Over a period of 6 months, while using their service, our income more than doubled and our customers have never been happier.",
@@ -23,28 +24,39 @@ const Testimonials = () => {
       role: "CTO at Craftly",
       photo: josh_photo,
       icon_class: "star",
-      icon: star_icon
+      icon: star_icon,
     },
     {
       text: "upTicket makes life at our startup so much easier and organized. It's simple, intuitive, and easy to use. Make yourself a favor and sign up for their services now. I wish I knew about them earlier.",
       name: "Kate Podolsky",
       role: "CEO at Digitio",
       photo: kate_photo,
-      icon_class: "plus-one",
-      icon: plus_one_icon
+      icon_class: "plus_one",
+      icon: plus_one_icon,
     },
   ];
 
   return (
-    <section className="testimonials-section" id="testimonials">
+    <section className={styles.testimonials_section} id="testimonials">
       <h2>
         Our goal is to make work within{" "}
-        <span className="red-text-span">small startups</span> simpler
+        <span className={styles.red_text}>small startups</span> simpler
       </h2>
-      <p className="testimonial-para">Just read what our customers say about us 🔥</p>
-      <div className="testimonials-container">
-        {testimonials.map((testimonial)=>{
-          return <SingleTestimonial text={testimonial.text} name={testimonial.name} role={testimonial.role} photo={testimonial.photo} icon={testimonial.icon} icon_class={testimonial.icon_class} />
+      <p className={styles.testimonial_para}>
+        Just read what our customers say about us 🔥
+      </p>
+      <div className={styles.testimonials_container}>
+        {testimonials.map((testimonial) => {
+          return (
+            <SingleTestimonial
+              text={testimonial.text}
+              name={testimonial.name}
+              role={testimonial.role}
+              photo={testimonial.photo}
+              icon={testimonial.icon}
+              icon_class={testimonial.icon_class}
+            />
+          );
         })}
       </div>
     </section>
