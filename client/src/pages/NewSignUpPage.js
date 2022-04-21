53e173ab14e7ec4bf24e71b12419ffa68fba1d33
mclbdn <michalbednar93@gmail.com>
2022-04-21 10:31:35 -0500

    Add logic to sign up page

--- client/src/pages/NewSignUpPage.js
+++ client/src/pages/NewSignUpPage.js
@@ -1,8 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import check from "../assets/check.svg";
 import styles from "./NewSignUpPage.module.scss";
 
 const NewSignUpPage = () => {
+  const navigate = useNavigate();
+  const [companyName, setCompanyName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [formHasErrors, setFormHasErrors] = useState(null);
+
+  useEffect(() => {
+    if (password !== confirmPassword) {
+      setFormHasErrors({
+        hasAnError: true,
+        errorMessageToShow: "Passwords must be matching.",
+      });
+    } else {
+      setFormHasErrors(null);
+    }
+  }, [password, confirmPassword]);
+
+  const registerUser = async (e) => {
+    e.preventDefault();
+
+    if (!formHasErrors) {
+      const response = await fetch("https://upticket-server.herokuapp.com/api/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          companyName,
+          email,
+          password,
+        }),
+      });
+
+      if (response.status === 200) {
+        navigate("/login");
+      } else {
+        setFormHasErrors({
+          hasAnError: true,
+          errorMessageToShow: "This e-mail is already registered.",
+        });
+      }
+    }
+  };
+
   return (
     <main className={styles.main}>
       <nav>
@@ -20,27 +66,59 @@ const NewSignUpPage = () => {
           <h1 className={styles.h1}>Register</h1>
           <p className={styles.under_header_para}>Manage all your tickets efficiently</p>
           <p className={styles.under_para_para}>Let's get you all set up!</p>
-          <form className={styles.form}>
+          <form onSubmit={registerUser} className={styles.form}>
             <div className={styles.top_form_container}>
               <div className={styles.left_form_container}>
                 <label htmlFor="companyName" className={styles.label}>
                   Company name
                 </label>
-                <input type="text" name="companyName" id="companyName" required minLength={2} maxLength={128} placeholder="Company name" />
+                <input
+                  value={companyName}
+                  onChange={(e) => setCompanyName(e.target.value)}
+                  type="text"
+                  name="companyName"
+                  id="companyName"
+                  required
+                  minLength={2}
+                  maxLength={128}
+                  placeholder="Company name"
+                />
                 <label htmlFor="companyEmail" className={styles.label}>
                   Company email
                 </label>
-                <input type="email" name="companyEmail" id="companyEmail" required minLength={4} maxLength={128} placeholder="Company email" />
+                <input
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  type="email"
+                  name="companyEmail"
+                  id="companyEmail"
+                  required
+                  minLength={4}
+                  maxLength={128}
+                  placeholder="Company email"
+                />
               </div>
               <div className={styles.right_form_container}>
                 <label htmlFor="password" className={styles.label}>
                   Password
                 </label>
-                <input type="password" name="password" id="password" required minLength={8} maxLength={128} placeholder="Password" />
+                <input
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  type="password"
+                  name="password"
+                  id="password"
+                  required
+                  minLength={8}
+                  maxLength={128}
+                  placeholder="Password"
+                />
                 <label htmlFor="repeatPassword" className={styles.label}>
                   Repeat password
                 </label>
                 <input
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
                   type="password"
                   name="repeatPassord"
                   id="repeatPassword"
