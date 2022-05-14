bc6500a7da813437aa2831bfcb0da8a6bade0913
mclbdn <michalbednar93@gmail.com>
2022-05-14 10:57:34 -0500

    Create a new build

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,7 @@ import { Provider } from "react-redux";
 
 const store = createStore(
   allReducers,
+  // Chrome extension settings
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
